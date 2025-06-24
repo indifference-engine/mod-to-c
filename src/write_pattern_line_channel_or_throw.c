ae3d60ebeac49dec625d47cebd53c7171b62fae9
siliconspecter <siliconspecter@outlook.com>
2025-06-24 17:48:41 +0100

    Implement portamento down.

--- src/write_pattern_line_channel_or_throw.c
+++ src/write_pattern_line_channel_or_throw.c
@@ -165,12 +165,12 @@ void write_pattern_line_channel_or_throw(const uint8_t pattern_index,
     break;
 
   case EFFECT_TYPE_COARSE_PORTAMENTO_DOWN:
-    // write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d)),\n",
-    //                coarse_portamento_effect_macro_name, song_name_macro_name,
-    //                song_name, pattern_index_macro_name, pattern_index,
-    //                line_index_macro_name, line_index,
-    //                channel_index_macro_name, channel_index,
-    //                periods_per_tick_macro_name, -effect_xy);
+    write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d), %s(%d))\n "
+                   "           )",
+                   coarse_portamento_effect_macro_name, song_name_macro_name,
+                   song_name, pattern_index_macro_name, pattern_index,
+                   line_index_macro_name, line_index, channel_index_macro_name,
+                   channel_index, periods_per_tick_macro_name, -effect_xy);
     break;
 
   case EFFECT_TYPE_TONE_PORTAMENTO:
