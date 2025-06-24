8318b90650bf5b139ddfb3267382fd5346e20c4d
siliconspecter <siliconspecter@outlook.com>
2025-06-24 23:42:47 +0100

    Implement fine portamento.

--- src/write_pattern_line_channel_or_throw.c
+++ src/write_pattern_line_channel_or_throw.c
@@ -350,21 +350,23 @@ void write_pattern_line_channel_or_throw(const uint8_t pattern_index,
       break;
 
     case EFFECT_TYPE_EXTRA_FINE_PORTAMENTO_UP:
-      // write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d)),\n",
-      //                fine_portamento_effect_macro_name, song_name_macro_name,
-      //                song_name, pattern_index_macro_name, pattern_index,
-      //                line_index_macro_name, line_index,
-      //                channel_index_macro_name, channel_index,
-      //                periods_macro_name, effect_y);
+      write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d), "
+                     "%s(%d))\n            )",
+                     fine_portamento_effect_macro_name, song_name_macro_name,
+                     song_name, pattern_index_macro_name, pattern_index,
+                     line_index_macro_name, line_index,
+                     channel_index_macro_name, channel_index,
+                     periods_macro_name, effect_y);
       break;
 
     case EFFECT_TYPE_EXTRA_FINE_PORTAMENTO_DOWN:
-      // write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d)),\n",
-      //                fine_portamento_effect_macro_name, song_name_macro_name,
-      //                song_name, pattern_index_macro_name, pattern_index,
-      //                line_index_macro_name, line_index,
-      //                channel_index_macro_name, channel_index,
-      //                periods_macro_name, -effect_y);
+      write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d), "
+                     "%s(%d))\n            )",
+                     fine_portamento_effect_macro_name, song_name_macro_name,
+                     song_name, pattern_index_macro_name, pattern_index,
+                     line_index_macro_name, line_index,
+                     channel_index_macro_name, channel_index,
+                     periods_macro_name, -effect_y);
       break;
 
     case EFFECT_TYPE_EXTRA_GLISSANDO:
