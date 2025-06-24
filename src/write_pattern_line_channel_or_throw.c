031325f9fb55b22764656a955346f2f79eace26a
siliconspecter <siliconspecter@outlook.com>
2025-06-24 23:32:39 +0100

    Add set filter.

--- src/write_pattern_line_channel_or_throw.c
+++ src/write_pattern_line_channel_or_throw.c
@@ -325,11 +325,11 @@ void write_pattern_line_channel_or_throw(const uint8_t pattern_index,
     case EFFECT_TYPE_EXTRA_SET_FILTER:
       switch (effect_y) {
       case 0:
-        // write_or_throw("              %s(%s(%s), %s(%d), %s(%d))\n",
-        //                enable_filter_effect_macro_name, song_name_macro_name,
-        //                song_name, pattern_index_macro_name, pattern_index,
-        //                line_index_macro_name, line_index,
-        //                channel_index_macro_name, channel_index);
+        write_or_throw(
+            "              %s(%s(%s), %s(%d), %s(%d))\n            )",
+            enable_filter_effect_macro_name, song_name_macro_name, song_name,
+            pattern_index_macro_name, pattern_index, line_index_macro_name,
+            line_index, channel_index_macro_name, channel_index);
         break;
 
       case 1:
