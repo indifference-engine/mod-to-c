96b07e96efde9b44e2a524e1d555b81444a5c275
siliconspecter <siliconspecter@outlook.com>
2025-06-25 22:02:02 +0100

    Add invert loop.

--- src/write_pattern_line_channel_or_throw.c
+++ src/write_pattern_line_channel_or_throw.c
@@ -639,19 +639,20 @@ void write_pattern_line_channel_or_throw(const uint8_t pattern_index,
 
     case EFFECT_TYPE_EXTRA_INVERT_LOOP:
       if (effect_y == 0) {
-        // write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d))\n",
-        //                invert_loop_effect_macro_name, song_name_macro_name,
-        //                song_name, pattern_index_macro_name, pattern_index,
-        //                line_index_macro_name, line_index,
-        //                channel_index_macro_name, channel_index,
-        //                delay_ticks_macro_name, effect_y);
+        write_or_throw(
+            "              %s(%s(%s), %s(%d), %s(%d), %s(%d))\n            )",
+            cancel_invert_loop_effect_macro_name, song_name_macro_name,
+            song_name, pattern_index_macro_name, pattern_index,
+            line_index_macro_name, line_index, channel_index_macro_name,
+            channel_index);
       } else {
-        // write_or_throw(
-        //     "              %s(%s(%s), %s(%d), %s(%d))\n",
-        //     cancel_invert_loop_effect_macro_name, song_name_macro_name,
-        //     song_name, pattern_index_macro_name, pattern_index,
-        //     line_index_macro_name, line_index, channel_index_macro_name,
-        //     channel_index);
+        write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d), "
+                       "%s(%d))\n            )",
+                       invert_loop_effect_macro_name, song_name_macro_name,
+                       song_name, pattern_index_macro_name, pattern_index,
+                       line_index_macro_name, line_index,
+                       channel_index_macro_name, channel_index,
+                       delay_ticks_macro_name, effect_y);
       }
       break;
     }
