08c40f108458108de7676dae7353ca1c34252503
siliconspecter <siliconspecter@outlook.com>
2025-06-25 21:50:04 +0100

    Implement note cut.

--- src/write_pattern_line_channel_or_throw.c
+++ src/write_pattern_line_channel_or_throw.c
@@ -608,12 +608,13 @@ void write_pattern_line_channel_or_throw(const uint8_t pattern_index,
       break;
 
     case EFFECT_TYPE_EXTRA_NOTE_CUT:
-      // write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d))\n",
-      //                note_cut_effect_macro_name, song_name_macro_name,
-      //                song_name, pattern_index_macro_name, pattern_index,
-      //                line_index_macro_name, line_index,
-      //                channel_index_macro_name, channel_index,
-      //                ticks_macro_name, effect_y);
+      write_or_throw("              %s(%s(%s), %s(%d), %s(%d), %s(%d), "
+                     "%s(%d))\n            )",
+                     note_cut_effect_macro_name, song_name_macro_name,
+                     song_name, pattern_index_macro_name, pattern_index,
+                     line_index_macro_name, line_index,
+                     channel_index_macro_name, channel_index, ticks_macro_name,
+                     effect_y);
       break;
 
     case EFFECT_TYPE_EXTRA_NOTE_DELAY:
